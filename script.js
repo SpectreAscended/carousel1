0aa7124f1ca0e4abc2e533c1d2d8bf2fffad5180
Cory Pollard <astandard87@gmail.com>
2022-05-17 20:38:26 -0600

    cleaned up some code

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ let curSlide = 1;
 imageBox.addEventListener('click', function(e) {
     let slide = +e.target.dataset.slide;
     if(!slide) return;
-    console.log(slide);
     slider = (-700 * slide) + 700;
     images.style.transform = `translateX(${slider}px)`;
     curSlide = slide;
